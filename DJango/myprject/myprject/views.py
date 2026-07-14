920e30fb332d8c499f7838f190efc6cada9c369c
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-07-14 12:42:02 +0530

    Django basic start

--- DJango/myprject/myprject/views.py
+++ DJango/myprject/myprject/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("it's the home page of the project.")
+    # return HttpResponse("it's the home page of the project.")
+    return render(request, 'website/index.html')
 def about(request):
     return HttpResponse("it's the about page of the project.")
 def contact(request):
