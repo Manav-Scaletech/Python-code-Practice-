89386b386d8a95cb0e51e79ee245cecacefd7c3e
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-07-15 16:54:31 +0530

    removing unwanted part

--- Module 2/ducktype.py
+++ Module 2/ducktype.py
@@ -21,12 +21,3 @@ duck = Duck()
 person = Person()   
 make_it_quack(duck)    # Output: Quack!
 make_it_quack(person)  # Output: I'm pretending to be a duck!
-
-
-[123678] -> dict{'odd' : [], 'even': []}
-
-
-
-
-
-a: list[int] = reduce( lambda x, y:  [1, 2, 3, 4])
