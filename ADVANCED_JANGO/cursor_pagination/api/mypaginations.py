dead43c24de6fdd9e8575b0c63df3ba4dbe30ba8
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-08-10 18:42:19 +0530

    Pagination topic ( cursor pagination ).

---
+++ ADVANCED_JANGO/cursor_pagination/api/mypaginations.py
@@ -0,0 +1,5 @@
+from rest_framework.pagination import CursorPagination  # <-- Add this import
+
+class MyCursorPagination(CursorPagination):             # <-- Tip: Rename your custom class so names don't conflict
+    page_size = 5
+    ordering = 'name'                                # <-- Set this to a valid field in your Student model
