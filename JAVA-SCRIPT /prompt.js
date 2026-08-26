6621aa5cfc863d084c4766b244ddf8c83dbf7ccd
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-08-26 15:30:37 +0530

    Prompt - alert - confirm.

---
+++ JAVA-SCRIPT /prompt.js
@@ -0,0 +1,12 @@
+//here is the basic code for the prompt , alert , confirm 
+
+
+a = confirm('are you the boss');
+console.log(a)
+
+
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+alert('Hello World') //one type of the pop-up 
