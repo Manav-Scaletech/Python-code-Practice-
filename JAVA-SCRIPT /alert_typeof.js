f0bb2f02b9de857c2767fd572fe486f397e057a4
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-08-26 14:17:38 +0530

    Java-script updation of it.

---
+++ JAVA-SCRIPT /alert_typeof.js
@@ -0,0 +1,10 @@
+console.log(typeof alert); 
+
+if (typeof alert === "function") {
+  alert("Hello World!");
+} else {
+  console.log("The alert function is not available in this environment.");
+}
+
+// typeof(alert) = function . 
+
