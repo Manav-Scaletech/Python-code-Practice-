2cf824d574f72b582a56ce127ca644ba216b41ba
Manav-Scaletech <manav.bagthaliya@scaletech.xyz>
2026-08-26 14:41:33 +0530

    variable one , var let and const.

---
+++ JAVA-SCRIPT /vaiables.js
@@ -0,0 +1,20 @@
+const a = 13;
+
+a = 145 ; // gives error 
+
+
+var  b = 12;
+
+var b = 123 ; // in var it's ok
+
+b = 111 ; // that is also ok 
+
+
+
+
+
+let c = 10;
+
+c = 1023; // ok. 
+
+// let c = 111; //not ok 
